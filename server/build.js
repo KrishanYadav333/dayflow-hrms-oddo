d16cba0221203432a7e455217266f4ad97ceb102
KrishanYadav333 <kryshan753@gmail.com>
2026-01-03 16:32:12 +0530

    Fix build script for Linux deployment

--- server/build.js
+++ server/build.js
@@ -8,14 +8,16 @@ execSync('cd ../client && npm install && npm run build', { stdio: 'inherit' });
 console.log('Copying build files...');
 // Create public directory if it doesn't exist
 if (!fs.existsSync('./public')) {
-  fs.mkdirSync('./public');
+  fs.mkdirSync('./public', { recursive: true });
 }
 
 // Copy build files to public directory
-if (process.platform === 'win32') {
-  execSync('xcopy /E /I /Y "..\\client\\build\\*" "public"', { stdio: 'inherit' });
-} else {
+try {
   execSync('cp -r ../client/build/* ./public/', { stdio: 'inherit' });
+  console.log('Build files copied successfully!');
+} catch (error) {
+  console.error('Error copying files:', error.message);
+  process.exit(1);
 }
 
 console.log('Build complete!');
