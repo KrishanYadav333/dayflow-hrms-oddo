907943c516cbe647084cb47b2585ea1d8dee7c50
KrishanYadav333 <kryshan753@gmail.com>
2026-01-03 16:19:54 +0530

    #10

--- server/build.js
+++ server/build.js
@@ -1,10 +1,21 @@
 const { execSync } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
 console.log('Building React app...');
 execSync('cd ../client && npm install && npm run build', { stdio: 'inherit' });
 
 console.log('Copying build files...');
-execSync('cp -r ../client/build ./public', { stdio: 'inherit' });
+// Create public directory if it doesn't exist
+if (!fs.existsSync('./public')) {
+  fs.mkdirSync('./public');
+}
+
+// Copy build files to public directory
+if (process.platform === 'win32') {
+  execSync('xcopy /E /I /Y "..\\client\\build\\*" "public"', { stdio: 'inherit' });
+} else {
+  execSync('cp -r ../client/build/* ./public/', { stdio: 'inherit' });
+}
 
 console.log('Build complete!');
